67d7ba96e8a1a705514e9a5a0e8e9e89823b713d
Jatan Rathod <rathodjatan4398@gmail.com>
2021-04-06 11:42:56 +0530

    UpdateOne

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -29,7 +29,7 @@ router.route("/update").post(function (req, res) {
     year: req.body.year,
   };
   console.log(doc);
-  Expense.update({ _id: req.body._id }, doc, function (err, result) {
+  Expense.updateOne({ _id: req.body._id }, doc, function (err, result) {
     if (err) res.send(err);
     res.send("Expense successfully updated!");
   });
